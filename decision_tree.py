5e8e8f0dd581419e81cda82ba105937145ae3434
Vincent Verdugo <38062516+vdugo@users.noreply.github.com>
2021-09-11 21:28:54 -0700

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
-# FOR: CS 4200- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# AUTHOR: Vincent Verdugo
+# FILENAME: decision_tree.py
+# SPECIFICATION: This program will read a dataset and output the ID3 (Iterative Dichotomiser 3) decision tree
+# FOR: CS 4210- Assignment #1
+# TIME SPENT: 50 minutes
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
@@ -15,6 +15,7 @@ import csv
 db = []
 X = []
 Y = []
+num_attributes = 4
 
 #reading the data in a csv file
 with open('contact_lens.csv', 'r') as csvfile:
@@ -24,13 +25,48 @@ with open('contact_lens.csv', 'r') as csvfile:
          db.append (row)
          print(row)
 
-#transform the original training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
-#--> add your Python code here
-# X =
+#transform the original training features to numbers and add to the 2D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
+# For each instance in the dataset
+for instance in db:
+  # Create a list, then run through each instance
+  replacement = []
+  for index, value in enumerate(instance):
+    # Replace each value with an arbitrary number
+    if index == 0:
+      if value == 'Young':
+        replacement.append(1)
+      elif value == 'Prepresbyopic':
+        replacement.append(2)
+      else:
+        replacement.append(3)
+    elif index == 1: 
+      if value == 'Myope':
+        replacement.append(1)
+      else:
+        replacement.append(2)
+    elif index == 2: 
+      if value == 'No':
+        replacement.append(1)
+      else:
+        replacement.append(2)
+    elif index == 3:
+      if value == 'Reduced':
+        replacement.append(1)
+      else:
+        replacement.append(2)
+  # append the instance replaced by integer values
+  X.append(replacement)
+# Now X will be the same as db, but each value for each instance is replaced by an arbitrary integer
+print(X)
 
 #transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
-#--> addd your Python code here
-# Y =
+# Go through every instance, only paying attention to the last column (the output variable)
+# append a 1 if it is a positive instance, otherwise append a 2 for a negative instance
+for instance in db:
+  if instance[num_attributes] == 'Yes':
+    Y.append(1)
+  else:
+    Y.append(2)
 
 #fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
